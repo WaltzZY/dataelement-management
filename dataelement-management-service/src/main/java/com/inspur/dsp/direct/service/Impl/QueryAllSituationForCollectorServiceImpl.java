74e8222b0e727b5c07b2715782daaddbd721d385
youhaibiao <chen_ll@inspur.com>
2025-09-23 14:09:43 +0800

    convert relevant attributes

--- dataelement-management-service/src/main/java/com/inspur/dsp/direct/service/Impl/QueryAllSituationForCollectorServiceImpl.java
+++ dataelement-management-service/src/main/java/com/inspur/dsp/direct/service/Impl/QueryAllSituationForCollectorServiceImpl.java
@@ -1,6 +1,7 @@
 package com.inspur.dsp.direct.service.Impl;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.inspur.dsp.direct.common.StatusUtil;
 import com.inspur.dsp.direct.dao.BaseDataElementMapper;
 import com.inspur.dsp.direct.domain.UserLoginInfo;
 import com.inspur.dsp.direct.entity.dto.BaseDataElementSearchDTO;
@@ -11,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -39,7 +39,7 @@ public class QueryAllSituationForCollectorServiceImpl implements QueryAllSituati
         baseDataElementSearchDTO.setOrgCode(orgCode);
         List<String> statusList = baseDataElementSearchDTO.getStatusList();
 
-        List<String>[] lists = buildStatusConditions(statusList);
+        List<String>[] lists = StatusUtil.buildStatusConditions(statusList);
         List<String> baseList = lists[0];
         List<String> taskList = lists[1];
         baseDataElementSearchDTO.setBaseStatusList(baseList);
@@ -49,66 +49,12 @@ public class QueryAllSituationForCollectorServiceImpl implements QueryAllSituati
         if (CollectionUtils.isEmpty(baseDataElementList)) {
             page.setRecords(Collections.emptyList());
         }
-        return page.setRecords(baseDataElementList);
-    }
-
-    /**
-     * 根据状态列表构建过滤条件
-     *
-     * @param statusList 状态列表
-     * @return 包含主表条件集合和从表条件集合的数组
-     */
-    public static List<String>[] buildStatusConditions(List<String> statusList) {
-        List<String> mainConditions = new ArrayList<>();
-        List<String> subConditions = new ArrayList<>();
-
-        if (statusList != null) {
-            for (String status : statusList) {
-                switch (status) {
-                    case "pending":
-                    case "pending_claimed":
-                        // 使用从表的status进行查询
-                        subConditions.add(status);
-                        break;
-                    case "confirmed":
-                        // 从表的过滤字段值为"待核定"，主表的过滤字段值为"已确认"
-                        subConditions.add("pending_approval");
-                        mainConditions.add("confirmed");
-                        break;
-                    case "rejected":
-                        // 从表的过滤字段值为"待协商"，主表的过滤字段值为"已拒绝"
-                        subConditions.add("pending_negotiation");
-                        mainConditions.add("rejected");
-                        break;
-
-                    case "claimed":
-                        // 主表的过滤字段值为"认领中或者待核定或者待协商"，从表的过滤字段值为"已认领"
-                        mainConditions.add("claiming");
-                        mainConditions.add("pending_approval");
-                        mainConditions.add("pending_negotiation");
-                        subConditions.add("claimed");
-                        break;
-
-                    case "not_claimed":
-                        // 主表的过滤字段值为"认领中或者待核定或者待协商"，从表的过滤字段值为"不认领"
-                        mainConditions.add("claiming");
-                        mainConditions.add("pending_approval");
-                        mainConditions.add("pending_negotiation");
-                        subConditions.add("not_claimed");
-                        break;
-                    case "negotiating":
-                    case "designated_source":
-                        // 使用主表的status进行查询
-                        mainConditions.add(status);
-                        break;
-                }
-            }
+        for (DataElementWithTaskVo dataElementWithTaskVo : baseDataElementList) {
+            String status = dataElementWithTaskVo.getStatus();
+            String statusChinese = StatusUtil.getStatusChinese(status);
+            dataElementWithTaskVo.setStatusChinese(statusChinese);
         }
-        // 返回包含两个集合的数组
-        List<String>[] result = new ArrayList[2];
-        result[0] = mainConditions;
-        result[1] = subConditions;
-        return result;
+        return page.setRecords(baseDataElementList);
     }
 
 
