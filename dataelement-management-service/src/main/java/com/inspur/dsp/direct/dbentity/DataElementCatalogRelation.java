b28c0a15ae64ea356c2db1257e864ae65af99de9
fanlinkai <1328697244@qq.com>
2025-10-30 13:13:11 +0800

    fix:修改表结构为正确结构

--- dataelement-management-service/src/main/java/com/inspur/dsp/direct/dbentity/DataElementCatalogRelation.java
+++ dataelement-management-service/src/main/java/com/inspur/dsp/direct/dbentity/DataElementCatalogRelation.java
@@ -4,14 +4,14 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-import java.io.Serializable;
 
 /**
  * 数据元-目录关联关系表
@@ -54,6 +54,13 @@ public class DataElementCatalogRelation implements Serializable {
     private String catalogName;
 
     /**
+     * 目录描述（摘要）
+     */
+    @TableField(value = "catalog_desc")
+    @Size(max = 500, message = "目录描述（摘要）最大长度要小于 500")
+    private String catalogDesc;
+
+    /**
      * 信息项ID
      */
     @TableField(value = "info_item_id")
@@ -66,6 +73,13 @@ public class DataElementCatalogRelation implements Serializable {
     @TableField(value = "info_item_name")
     @Size(max = 100, message = "信息项名称最大长度要小于 100")
     private String infoItemName;
+
+    /**
+     * 信息项类型
+     */
+    @TableField(value = "Info_item_datatype")
+    @Size(max = 20, message = "信息项类型最大长度要小于 20")
+    private String infoItemDatatype;
 
     /**
      * 目录单位统一社会信用代码
@@ -81,4 +95,29 @@ public class DataElementCatalogRelation implements Serializable {
     @Size(max = 200, message = "目录单位名称最大长度要小于 200")
     private String catalogUnitName;
 
+    /**
+     * 创建日期
+     */
+    @TableField(value = "create_date")
+    private Date createDate;
+
+    /**
+     * 创建人账号
+     */
+    @TableField(value = "create_account")
+    @Size(max = 30, message = "创建人账号最大长度要小于 30")
+    private String createAccount;
+
+    /**
+     * 最后修改日期
+     */
+    @TableField(value = "last_modify_date")
+    private Date lastModifyDate;
+
+    /**
+     * 最后修改人账号
+     */
+    @TableField(value = "last_modify_account")
+    @Size(max = 30, message = "最后修改人账号最大长度要小于 30")
+    private String lastModifyAccount;
 }
