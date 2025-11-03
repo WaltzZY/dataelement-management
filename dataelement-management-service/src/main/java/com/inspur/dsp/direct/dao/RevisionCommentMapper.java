9e22bec86168ab2c4d7dcd03058e093d4d68a49a
fanlinkai <1328697244@qq.com>
2025-11-03 15:43:29 +0800

    fate:修订意见记录表基础类

---
+++ dataelement-management-service/src/main/java/com/inspur/dsp/direct/dao/RevisionCommentMapper.java
@@ -0,0 +1,9 @@
+package com.inspur.dsp.direct.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.inspur.dsp.direct.dbentity.RevisionComment;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RevisionCommentMapper extends BaseMapper<RevisionComment> {
+}
